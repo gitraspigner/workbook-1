525d640a9284c3f52d5760279b5a7f710083286d
raspi <gitraspigner@gmail.com>
2025-09-24 15:50:28 -0700

    Adding miniInputExcercise exercise for learning Scanner class for input

---
+++ src/com/pluralsight/miniInputExcercise.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+import java.util.Scanner;
+
+
+public class miniInputExcercise {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is your favorite animal?: ");
+        String favoriteAnimal = input.nextLine();
+        System.out.print("What the first tool you can think of that comes to mind?: ");
+        String firstTool = input.nextLine();
+        System.out.print("What is the number (day) of your birthday?: ");
+        int bdayNumber = input.nextInt();
+
+        System.out.println("Your favorite animal is a: " + favoriteAnimal + ", the first tool " +
+                "that comes to mind is a: " + firstTool + ", and " + bdayNumber + " is the day " +
+                "you were born.");
+
+    }
+}
