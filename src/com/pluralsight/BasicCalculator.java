988caf222efcd906e041c947c46feb90fdad586c
raspi <gitraspigner@gmail.com>
2025-09-25 10:30:52 -0700

    (Working ahead) adding BasicCalculator(part 2- other operators using conditional statements)

--- src/com/pluralsight/BasicCalculator.java
+++ src/com/pluralsight/BasicCalculator.java
@@ -23,22 +23,28 @@ public class BasicCalculator {
         String operatorInput = scanner.nextLine().toUpperCase(); //string to hold user input
         double result;
         String operatorString = ""; //string to hold operator to be printed in result expression
-        if (Objects.equals(operatorInput, "A")) {
-            result = n1 + n2;
-            operatorString = "+";
-        } else if (Objects.equals(operatorInput, "S")){
-            result = n1 - n2;
-            operatorString = "-";
-        } else if (Objects.equals(operatorInput, "M")){
-            result = n1 * n2;
-            operatorString = "*";
-        } else if (Objects.equals(operatorInput, "D")){
-            result = (double) n1 / n2;
-            operatorString = "/";
-        } else {
-            result = 0.0;
-            System.out.println("Invalid Operator Selected.");
-            System.out.println("Please Run Again.");
+        switch (operatorInput) { //switch will use String.equals which is wanted
+            case "A":
+                result = n1 + n2;
+                operatorString = "+";
+                break;
+            case "S":
+                result = n1 - n2;
+                operatorString = "-";
+                break;
+            case "M":
+                result = n1 * n2;
+                operatorString = "*";
+                break;
+            case "D":
+                result = (double) n1 / n2;
+                operatorString = "/";
+                break;
+            default:
+                result = 0.0;
+                System.out.println("Invalid Operator Selected.");
+                System.out.println("Please Run Again.");
+                break;
         }
         System.out.println("Result: ");
         System.out.println(n1 + " " + operatorString + " " + n2 + " = " + result);
